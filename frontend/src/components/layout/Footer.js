bf3db1e2af33f7c5764a7a167acfc87f4087316b
willtoch <wtochukwu@yahoo.com>
2021-04-20 03:06:44 +0100

    Add client folder back to git repo

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+          <footer className="py-1">
+                    <p className="text-center mt-1">
+                        medic-online 2020-2020, All Rights Reserved
+                    </p>
+             </footer>
+        </Fragment>
+    )
+}
+
+export default Footer

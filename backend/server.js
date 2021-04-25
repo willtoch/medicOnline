9c8cfc2f3ec5828b84fa19fbc523ef5b99322b1f
willtoch <wtochukwu@yahoo.com>
2021-04-25 01:42:01 +0100

    Server modified

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ process.on('uncaughtException', err => {
 
 //Setting up config file
 if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({path: 'backend/config/config.env'})
+// dotenv.config({ path: 'backend/config/config.env' })
+
 
 
 // Connecting to database
